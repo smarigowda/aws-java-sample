bc027e1138a92a470911846ae3b5f4f37fd6e845
Santosh <santosharakere@gmail.com>
2017-02-10 15:50:32 +0000

    list nft result files

--- src/main/java/com/amazonaws/samples/S3Sample.java
+++ src/main/java/com/amazonaws/samples/S3Sample.java
@@ -14,29 +14,18 @@
  */
 package com.amazonaws.samples;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.util.UUID;
-
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ListObjectsRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+import java.io.*;
+import java.util.UUID;
 
 /**
  * This sample demonstrates how to make basic requests to Amazon S3 using
@@ -54,7 +43,7 @@ public class S3Sample {
 
     public static void main(String[] args) throws IOException {
         /*
-         * Create your credentials file at ~/.aws/credentials (C:\Users\USER_NAME\.aws\credentials for Windows users) 
+         * Create your credentials file at ~/.aws/credentials (C:\Users\USER_NAME\.aws\credentials for Windows users)
          * and save the following lines after replacing the underlined values with your own.
          *
          * [default]
@@ -63,8 +52,9 @@ public class S3Sample {
          */
 
         AmazonS3 s3 = new AmazonS3Client();
-        Region usWest2 = Region.getRegion(Regions.US_WEST_2);
-        s3.setRegion(usWest2);
+//        Region usWest2 = Region.getRegion(Regions.US_WEST_2);
+        Region euWest1 = Region.getRegion(Regions.EU_WEST_1);
+        s3.setRegion(euWest1);
 
         String bucketName = "my-first-s3-bucket-" + UUID.randomUUID();
         String key = "MyObjectKey";
@@ -82,17 +72,17 @@ public class S3Sample {
              * You can optionally specify a location for your bucket if you want to
              * keep your data closer to your applications or users.
              */
-            System.out.println("Creating bucket " + bucketName + "\n");
-            s3.createBucket(bucketName);
+//            System.out.println("Creating bucket " + bucketName + "\n");
+//            s3.createBucket(bucketName);
 
             /*
              * List the buckets in your account
              */
-            System.out.println("Listing buckets");
-            for (Bucket bucket : s3.listBuckets()) {
-                System.out.println(" - " + bucket.getName());
-            }
-            System.out.println();
+//            System.out.println("Listing buckets");
+//            for (Bucket bucket : s3.listBuckets()) {
+//                System.out.println(" - " + bucket.getName());
+//            }
+//            System.out.println();
 
             /*
              * Upload an object to your bucket - You can easily upload a file to
@@ -102,8 +92,8 @@ public class S3Sample {
              * like content-type and content-encoding, plus additional metadata
              * specific to your applications.
              */
-            System.out.println("Uploading a new object to S3 from a file\n");
-            s3.putObject(new PutObjectRequest(bucketName, key, createSampleFile()));
+//            System.out.println("Uploading a new object to S3 from a file\n");
+//            s3.putObject(new PutObjectRequest(bucketName, key, createSampleFile()));
 
             /*
              * Download an object - When you download an object, you get all of
@@ -117,10 +107,10 @@ public class S3Sample {
              * conditional downloading of objects based on modification times,
              * ETags, and selectively downloading a range of an object.
              */
-            System.out.println("Downloading an object");
-            S3Object object = s3.getObject(new GetObjectRequest(bucketName, key));
-            System.out.println("Content-Type: "  + object.getObjectMetadata().getContentType());
-            displayTextInputStream(object.getObjectContent());
+//            System.out.println("Downloading an object");
+//            S3Object object = s3.getObject(new GetObjectRequest(bucketName, key));
+//            System.out.println("Content-Type: "  + object.getObjectMetadata().getContentType());
+//            displayTextInputStream(object.getObjectContent());
 
             /*
              * List objects in your bucket by prefix - There are many options for
@@ -130,30 +120,53 @@ public class S3Sample {
              * use the AmazonS3.listNextBatchOfObjects(...) operation to retrieve
              * additional results.
              */
+//            System.out.println("Listing objects");
+//            ObjectListing objectListing = s3.listObjects(new ListObjectsRequest()
+//                    .withBucketName(bucketName));
+////                    .withPrefix("My"));
+//            for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+//                System.out.println(" - " + objectSummary.getKey() + "  " +
+//                        "(size = " + objectSummary.getSize() + ")");
+//            }
+//            System.out.println();
+
+            // list all the objects under bucket name nftresults
             System.out.println("Listing objects");
-            ObjectListing objectListing = s3.listObjects(new ListObjectsRequest()
-                    .withBucketName(bucketName)
-                    .withPrefix("My"));
-            for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
-                System.out.println(" - " + objectSummary.getKey() + "  " +
-                        "(size = " + objectSummary.getSize() + ")");
+
+            ObjectListing objectListing2 = s3.listObjects(new ListObjectsRequest()
+//                    .withBucketName("my-first-s3-bucket-d2f4e921-aec2-48e5-b811-edfed552f6f3"));
+                    .withBucketName("nftresults")
+                    .withPrefix("download/Result")
+                    .withDelimiter("/"));
+
+//            for (String commonPrefix : objectListing2.getCommonPrefixes()) {
+//                System.out.println(commonPrefix);
+//            }
+
+            for (S3ObjectSummary objectSummary : objectListing2.getObjectSummaries()) {
+                if (objectSummary.getKey().matches(".*Feb_10.*")) {
+                    System.out.println(" - " + objectSummary.getKey() + "  " +
+                            "(size = " + objectSummary.getSize() + ")");
+                }
             }
+
             System.out.println();
+
 
             /*
              * Delete an object - Unless versioning has been turned on for your bucket,
              * there is no way to undelete an object, so use caution when deleting objects.
              */
-            System.out.println("Deleting an object\n");
-            s3.deleteObject(bucketName, key);
+//            System.out.println("Deleting an object\n");
+//            s3.deleteObject(bucketName, key);
 
             /*
              * Delete a bucket - A bucket must be completely empty before it can be
              * deleted, so remember to delete any objects from your buckets before
              * you try to delete them.
              */
-            System.out.println("Deleting bucket " + bucketName + "\n");
-            s3.deleteBucket(bucketName);
+//            System.out.println("Deleting bucket " + bucketName + "\n");
+//            s3.deleteBucket(bucketName);
         } catch (AmazonServiceException ase) {
             System.out.println("Caught an AmazonServiceException, which means your request made it "
                     + "to Amazon S3, but was rejected with an error response for some reason.");
